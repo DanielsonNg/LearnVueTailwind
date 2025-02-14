602087d2b804719da619693351608f3870cb4ac3
unknown <danielson7632@gmail.com>
2025-02-14 09:42:21 +0700

    setup with routing

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex">
+      <div >
+          test
+      </div>
+      <div>
+        test
+      </div>    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
